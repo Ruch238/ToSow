93eb8a736d5430eec531e9cf54509eaa8f2bd2d0
Rucha <pooja851995>
2021-10-29 18:09:07 +0530

    First commit

---
+++ Crop_Prediction_Model (2)/Crop_Prediction_Model/views.py
@@ -0,0 +1,38 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import joblib
+
+def home(request):
+    return render(request, "home.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def ourteams(request):
+    return render(request, "ourteams.html") 
+
+def ourservices(request):
+    return render(request, "ourservices.html")
+    
+def result(request):
+
+    CL = joblib.load("FinalModel.sav")
+
+    lst = []
+
+    lst.append(float(request.GET["N"]))
+    lst.append(float(request.GET["P"]))
+    lst.append(float(request.GET["K"]))
+    lst.append(float(request.GET["temperature"]))
+    lst.append(float(request.GET["ph"]))
+    lst.append(float(request.GET["rainfall"]))
+
+    print(lst)
+
+    ans = CL.predict([lst])
+
+    A = ans.tolist()
+    ANS = ' '.join(map(str, A))
+    print(ANS)
+
+    return render(request, "result.html", {"ans": ANS, "lst": lst})
